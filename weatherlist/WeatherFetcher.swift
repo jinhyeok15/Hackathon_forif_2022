9bf296043d579bd44c5f42781f1b7fb7c36a5404
nick-sketchsoft <nick@sketchsoft3d.com>
2022-06-25 14:02:22 +0900

    fetch weather

---
+++ weatherlist/WeatherFetcher.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherFetcher.swift
+//  weatherlist
+//
+//  Created by Jiwon Park on 2022/06/25.
+//
+
+import UIKit
+
+class WeatherFetcher {
+    static func fetchWeather(city: String, completion: @escaping (Weather?) -> Void) {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=6949a3ca522322494cdcc85fc6060ddc&units=metric")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let weather = try decoder.decode(Weather.self, from: data)
+                completion(weather)
+            } catch {
+                print(error)
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+}
--- weatherlist/weatherlist/ContentView.swift
+++ weatherlist/weatherlist/ContentView.swift
@@ -8,29 +8,117 @@
 import SwiftUI
 
 struct ContentView: View {
-    struct EventDetailView: View {
-        var event: Event
-        
-        var body: some View {
-            Text(event.name)
-            Text(event.date, style: .date)
-        }
-    }
-    
+
     struct Event: Identifiable {
         var id = UUID()
         var name: String
         var date: Date
+        var location: String
+        var description: String = ""
+        var weather: Weather?
+    }
+
+    // private var events = [
+    //     Event(
+    //         name: "서울 재즈페스티벌",
+    //         date: Calendar.current.date(from:DateComponents(year: 2022, month: 05, day: 14))!,
+    //         location: "서울시 중구 을지로대로 삼거리",
+    //         description: "서울시 중구 을지로대로 삼거리에서 재즈페스티벌을 진행합니다."
+    //         ),
+    //     Event(
+    //         name: "The Airhouse in Gapyeong",
+    //         date: Calendar.current.date(from:DateComponents(year: 2022, month: 05, day: 15))!,
+    //         location: "강원도 가평군 가평읍 가평리",
+    //         description: "강원도 가평군 가평읍 가평리에서 The Airhouse를 진행합니다."
+    //         ),
+    //     Event(
+    //         name: "Surf! - 소란 여름콘서트",
+    //         date: Calendar.current.date(from:DateComponents(year: 2022, month: 05, day: 16))!,
+    //         location: "노들섬",
+    //         description: "노들섬에서 Surf! 소란 여름콘서트를 진행합니다."
+    //         ),
+    // ]
+
+    struct EventInputForm: View {
+        @State var eventName: String = "";
+        @State var eventDate: Date = Date();
+        @State var eventLocation: String = "";
+        @State var eventDescription: String = "";
+
+        @Environment(\.presentationMode) var presentationMode
+
+        // events data reference
+        @Binding var events: [Event]
+
+        var body: some View {
+            // input event info
+            NavigationView {
+                Form {
+                    Section {
+                        TextField("Name", text: $eventName)
+                        DatePicker("Date", selection: $eventDate)
+                        TextField("Location", text: $eventLocation)
+                        TextField("Description", text: $eventDescription)
+                    }
+                    Section {
+                        Button("Add Event") {
+                            if (self.eventName == "") { return }
+                            // add event to events var
+                            self.events.append(Event(
+                                name: self.eventName,
+                                date: self.eventDate,
+                                location: self.eventLocation,
+                                description: self.eventDescription
+                            ))
+                            // clear state
+                            self.eventName = "";
+                            self.eventDate = Date();
+                            self.eventLocation = "";
+                            self.eventDescription = "";
+
+                            // close current view
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+                .navigationBarTitle("Add Event")
+            }
+        }
     }
     
-    private var events = [
-        Event(name: "서울 재즈페스티벌", date: Calendar.current.date(from:DateComponents(year: 2022, month: 05, day: 14))!),
-        Event(name: "The Airhouse in Gapyeong", date: Date()),
-        Event(name: "Surf! - 소란 여름콘서트", date: Date())
-    ]
+    struct EventDetailView: View {
+        @State private var weather: Weather? = nil;
+        @State private var isLoading: Bool = true;
+
+        var event: Event
+        
+        var body: some View {
+            // fetch and print weather
+            WeatherFetcher.fetchWeather(city: event.location) { weather in
+                self.weather = weather
+                self.isLoading = false
+            }
+            return NavigationView {
+                VStack {
+                    Text(event.name)
+                    Text(event.date, style: .date)
+                    Text(event.location)
+                    Text(event.description)
+                    if (self.isLoading) {
+                        Text("Loading...")
+                    } else if (self.weather != nil) {
+                        Text(self.weather!.description)
+                    } else {
+                        Text("No weather data")
+                    }
+                }
+            }
+        }
+    }
     
     @State private var multiSelection = Set<UUID>();
     @State private var showingTodoInput = false;
+    @State private var events = [Event]();
     
     var body: some View {
         NavigationView {
@@ -46,13 +134,10 @@ struct ContentView: View {
                 .navigationTitle("Events")
                 .toolbar {
                     ToolbarItem(placement: .automatic) {
-                        Button(
-                            action: {
-                                showingTodoInput = true
-                            },
-                            label: {
+                        NavigationLink(destination: EventInputForm(events: 
+                            $events)) {
                             Image(systemName: "plus")
-                        })
+                        }
                     }
                 }
                 .toolbar {
